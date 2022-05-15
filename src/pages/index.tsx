ccfe36e37b16f70dd9b36b877ca5ccbb89f90c5f
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2022-05-15 04:36:29 -0300

    homepage

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,17 @@
 import type { NextPage } from "next";
 import Head from "next/head";
-import { Header } from "../Header";
-import { HeroImage } from "../HeroImage";
+import { Header } from "../components/Header";
+import { HeroImage } from "../components/HeroImage";
+import { ServicesComponent } from "../components/ServicesComponent";
+import { Status } from "../components/Status";
 
 const Home: NextPage = () => {
   return (
     <div >
       <Header />
       <HeroImage/>
+      <Status/>
+      <ServicesComponent/>
     </div>
   );
 };
