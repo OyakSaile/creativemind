f4b1996edee476a363107b73e8998b494490f396
Kayo Elias <46985165+OyakSaile@users.noreply.github.com>
2022-05-15 04:35:44 -0300

    status

---
+++ src/components/Status/index.tsx
@@ -0,0 +1,39 @@
+export function Status() {
+  return (
+    <section className=" bg-primary h-40 my-20 flex justify-center items-center">
+      <div className="flex items-center container mx-auto text-white gap-8">
+        <div className="flex items-center justify-center gap-4">
+          <span className="text-6xl font-bold ">3</span>
+          <p className="w-3/4 text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center gap-4">
+          <span className="text-6xl font-bold ">1%</span>
+          <p className="w-3/4 text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center gap-4">
+          <span className="text-6xl font-bold ">15</span>
+          <p className="w-3/4 text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+
+        <div className="flex items-center justify-center gap-4">
+          <span className="text-6xl font-bold ">2K</span>
+          <p className="w-3/4 text-justify">
+            Lorem Ipsum is simply dummy text of the printing and typesetting
+            industry.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+}
